7b14cf6bc4b3ff034b65b8b7b28e5a7054fd4a7a
Dominik Malinowski <dominik.malinowski@dxc.com>
2023-10-31 16:27:49 +0100

    Add home/register/nav component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './home/nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './home/register/register.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
